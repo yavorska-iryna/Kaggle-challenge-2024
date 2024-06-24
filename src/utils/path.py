585c3179b979b5683e9cd75b5e2191d512daae1b
dougollerenshaw <d.ollerenshaw@gmail.com>
2024-06-23 18:09:50 -0700

    Added some docstrings

--- src/utils/path.py
+++ src/utils/path.py
@@ -2,11 +2,29 @@ import os
 import git
 
 def get_repo_dir():
+    """
+    Get the path to the root of the repo dir
+
+    Args:
+        None
+    
+    Returns:
+        str
+    """
     # Get the git repo
     repo = git.Repo(os.path.dirname(os.path.abspath(__file__)), search_parent_directories=True)
     # Return the root path
     return repo.git.rev_parse("--show-toplevel")
 
-def get_data_dir():
+def get_data_dir(): 
+    """
+    Get the path to the data directory
+
+    Args:
+        None
+    
+    Returns:
+        str
+    """
     repo_dir = get_repo_dir()
     return os.path.join(repo_dir, 'data')
